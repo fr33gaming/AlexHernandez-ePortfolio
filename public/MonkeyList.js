a6e6c3be36a935e132666fce0a6e3f288d564106
fr33gaming <fr33gaming209@gmail.com>
2025-02-24 07:13:14 -0800

    Delete public directory

--- public/MonkeyList.js
+++
@@ -1,5 +0,0 @@
-const monkeys = [
-    { name: "Max", species: "Capuchin", gender: "Male", age: "2", weight: "20", tailLength: "12", height: "12", bodyLength: "12", acquisitionDate: "2025-02-08", acquisitionLocation: "United States", trainingStatus: "Intake", reserved: false, inServiceLocation: "United States"}
-];
-
-export { monkeys };
